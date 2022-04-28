2478431fcf72b3920bb7ff240ba734449b08ef74
Bryan Niwa <bniwa@sfu.ca>
2022-04-28 00:21:19 -0700

    Perform multiple readings over sample period

--- air-pollutant.py
+++ air-pollutant.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import time
 
 API_TOKEN =
 
@@ -37,16 +38,34 @@ def get_station_pm25(station_id):
     return pm25
 
 
+def read_all_stations(stations):
+    readings = []
+    for station in stations:
+        pm25 = get_station_pm25(station.uid)
+        readings.append(pm25)
+        print(f'{station.name}: {pm25}')
+    return readings
+
+
 if len(sys.argv) < 5:
-    print('Usage: air-pollutant.py <lat1> <long1> <lat2> <long2> [sample_period (minutes)] [sample_rate (minutes)]')
+    print('Usage: air-pollutant.py <lat1> <lng1> <lat2> <lng2> [sample_period (minutes)] [sample_rate (minutes)]')
     sys.exit(1)
 
-stations = get_stations((float(sys.argv[1]), float(sys.argv[2])), (float(sys.argv[3]), float(sys.argv[4])))
+lat1, lng1, lat2, lng2 = [float(arg) for arg in sys.argv[1:5]]
+sample_period_sec = int(sys.argv[5])*60 if len(sys.argv) > 5 else 5*60
+sample_rate_sec = int(sys.argv[6])*60 if len(sys.argv) > 6 else 1*60
+
+stations = get_stations((lat1, lng1), (lat2, lng2))
 readings  = []
 
-for station in stations:
-    pm25 = get_station_pm25(station.uid)
-    readings.append(pm25)
-    print(f'{station.name}: {pm25}')
+for i in range(sample_period_sec // sample_rate_sec):
+    current_readings = read_all_stations(stations)
+    readings += current_readings
+    print('\n\n')
+    time.sleep(sample_rate_sec)
+
+current_readings = read_all_stations(stations)
+readings += current_readings
+print('\n\n')
 
 print(f'Average: {sum(readings) / len(readings)}')
