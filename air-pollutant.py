c50de80469bc320528eb3b2f4cfccab4b96fac0f
Bryan Niwa <bniwa@sfu.ca>
2022-04-27 23:56:12 -0700

    Represent a station with a simple class
    We want to keep track of a station's name and uid so keep them in a class

--- air-pollutant.py
+++ air-pollutant.py
@@ -3,7 +3,12 @@ import sys
 
 API_TOKEN =
 
-def get_station_ids(coord_1, coord_2):
+class Station:
+    def __init__(self, name, uid):
+        self.name = name
+        self.uid = uid
+
+def get_stations(coord_1, coord_2):
     res = requests.get(
         f'http://api.waqi.info/map/bounds?token={API_TOKEN}&latlng={coord_1[0]},{coord_1[1]},{coord_2[0]},{coord_2[1]}')
 
@@ -12,8 +17,11 @@ def get_station_ids(coord_1, coord_2):
         sys.exit(1)
     
     station_data = res.json()['data']
-    station_ids = [item['uid'] for item in station_data]
-    return station_ids
+    stations = []
+    for item in station_data:
+        station = Station(item['station']['name'], item['uid'])
+        stations.append(station)
+    return stations
 
 
 def get_station_pm25(station_id):
@@ -33,8 +41,12 @@ if len(sys.argv) < 5:
     print('Usage: air-pollutant.py <lat1> <long1> <lat2> <long2> [sample_period (minutes)] [sample_rate (minutes)]')
     sys.exit(1)
 
-station_ids = get_station_ids((float(sys.argv[1]), float(sys.argv[2])), (float(sys.argv[3]), float(sys.argv[4])))
+stations = get_stations((float(sys.argv[1]), float(sys.argv[2])), (float(sys.argv[3]), float(sys.argv[4])))
+readings  = []
 
-for station_id in station_ids:
-    pm25 = get_station_pm25(station_id)
-    print(pm25)
+for station in stations:
+    pm25 = get_station_pm25(station.uid)
+    readings.append(pm25)
+    print(f'{station.name}: {pm25}')
+
+print(f'Average: {sum(readings) / len(readings)}')
