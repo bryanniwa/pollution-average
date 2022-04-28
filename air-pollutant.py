0e143dc814aa2f786d8d75560878bdc706226a02
Bryan Niwa <bniwa@sfu.ca>
2022-04-27 23:36:04 -0700

    Get pm25 values for all stations

--- air-pollutant.py
+++ air-pollutant.py
@@ -16,9 +16,25 @@ def get_station_ids(coord_1, coord_2):
     return station_ids
 
 
+def get_station_pm25(station_id):
+    res = requests.get(
+        f'http://api.waqi.info/feed/@{station_id}/?token={API_TOKEN}')
+
+    if res.status_code != 200:
+        print('Error:', res.status_code)
+        sys.exit(1)
+
+    station_data = res.json()['data']
+    pm25 = station_data['iaqi']['pm25']['v']
+    return pm25
+
+
 if len(sys.argv) < 5:
     print('Usage: air-pollutant.py <lat1> <long1> <lat2> <long2> [sample_period (minutes)] [sample_rate (minutes)]')
     sys.exit(1)
 
 station_ids = get_station_ids((float(sys.argv[1]), float(sys.argv[2])), (float(sys.argv[3]), float(sys.argv[4])))
-print(station_ids)
+
+for station_id in station_ids:
+    pm25 = get_station_pm25(station_id)
+    print(pm25)
